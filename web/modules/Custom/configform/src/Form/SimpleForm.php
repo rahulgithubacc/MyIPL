0c2ab05567d54859ba3d9bd6461a3ccf4ba3804b
rahulqed <rahul.deshmukh324.rdr@gmail.com>
2019-02-27 10:42:55 +0530

    Added updates files with validation

--- web/modules/Custom/configform/src/Form/SimpleForm.php
+++ web/modules/Custom/configform/src/Form/SimpleForm.php
@@ -2,10 +2,10 @@
 
 namespace Drupal\configform\Form;
  
-use Drupal\Core\Form\ConfigFormBase;
+use Drupal\Core\Form\FormBase;
 use Drupal\Core\Form\FormStateInterface;
  
-class SimpleConfigForm extends ConfigFormBase {
+class SimpleForm extends FormBase{
  
   /**
  
@@ -27,17 +27,17 @@ class SimpleConfigForm extends ConfigFormBase {
  
   public function buildForm(array $form, FormStateInterface $form_state) {
  
-    $form = parent::buildForm($form, $form_state);
+    // $form = parent::buildForm($form, $form_state);
  
-    $config = $this->config('custom.settings');
+    // $config = $this->config('custom.settings');
  
     $form['email'] = array(
  
-      '#type' => 'textfield',
+      '#type' => 'email',
  
       '#title' => $this->t('Email'),
  
-      '#default_value' => $config->get('custom.email'),
+    //   '#default_value' => $config->get('custom.email'),
  
       '#required' => TRUE,
  
@@ -48,23 +48,27 @@ class SimpleConfigForm extends ConfigFormBase {
  
       '#title' => $this->t('Name'),
  
-      '#default_value' => $config->get('custom.name'),
+    //   '#default_value' => $config->get('custom.name'),
  
       '#required' => TRUE,
  
     );
- 
-    
- 
+    $form['submit']=array(
+        '#type' =>'submit',
+        '#value' =>'Save',
+        '#required' =>TRUE,
+    );
     return $form;
  
   }
+
   public function validateForm(array &$form,FormStateInterface $form_state){
     $name = $form_state->getValue('name');
     if(!preg_match("/^([a-zA-Z]+)$/",$name)){
       $form_state->setErrorByName('name',$this->t('Enter a valid name.Name contains only letters'));
     }
   }
+ 
   /**
  
    * {@inheritdoc}
@@ -72,17 +76,9 @@ class SimpleConfigForm extends ConfigFormBase {
    */
  
   public function submitForm(array &$form, FormStateInterface $form_state) {
-    $values = $form_state->getValues();
-    $this->config('custom.settings')
-    ->set('email', $form_state->getValue('email'))
-    ->set('name', $form_state->getValue('name'))
- 
-    ->save();
- 
-  parent::submitForm($form, $form_state);
-
- 
-  }
+    \Drupal::messenger()->addMessage(t('Name :'. $form_state->getValue('name')));
+    \Drupal::messenger()->addMessage(t('Email :'. $form_state->getValue('email')));
+    }
  
   /**
  
