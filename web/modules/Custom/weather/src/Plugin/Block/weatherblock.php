e4a286c2866458953c0438a6c21201fb28f783cd
rahulqed <rahul.deshmukh324.rdr@gmail.com>
2019-03-26 19:19:15 +0530

    Worked on weather API 2nd day

--- web/modules/Custom/weather/src/Plugin/Block/weatherblock.php
+++ web/modules/Custom/weather/src/Plugin/Block/weatherblock.php
@@ -3,11 +3,13 @@
 namespace Drupal\weather\Plugin\Block;
 use Drupal\Core\Block\BlockBase;
 use Drupal\Core\Form\FormInterface;
-
+use Drupal\Core\Form\FormStateInterface;
+use Drupal\Component\Serialization\Json;
+use Drupal\Core\Entity;
 /**
  * @Block(
  *   id = "weather_block",
- *   admin_label = @Translation("pune block"),
+ *   admin_label = @Translation("weather block"),
  *   category = @Translation("City weather")
  * )
  */
@@ -15,40 +17,61 @@ class weatherblock extends BlockBase {
   /**
    * {@inheritdoc}
    */
-  public function build() {
-    $appid=\Drupal::service('weather.settings')->get('appid');
-    print_r($appid);
-    $form['wind -> speed'] = array(
+
+
+
+   public function blockForm($form,FormStateInterface $form_state)
+   {
+
+     $form['city'] = array(
  
         '#type' => 'textfield',
    
-        '#title' => $this->t('wind -> speed'),
+        '#title' => $this->t('city'),
       );
-      $form['temp_max'] = array(
+      $form['desc'] = array(
  
         '#type' => 'textfield',
    
-        '#title' => $this->t('temp_max'),
+        '#title' => $this->t('desc'),
       );
-      $form['pressure'] = array(
+      $form['img'] = array(
  
-        '#type' => 'number',
+        '#type' => 'managed_file',
    
-        '#title' => $this->t('pressure'),
+        '#title' => $this->t('img'),
       );
-      $form['humidity'] = array(
- 
-        '#type' => 'textfield',
-   
-        '#title' => $this->t('humidity'),
-        
-      );
-      $form['temp_min'] = array(
- 
-        '#type' => 'textfield',
-   
-        '#title' => $this->t('temp_min'),
-      );
-    return $form;
+     
+      return $form;
    }
+   public function blockSubmit($form,FormStateInterface $form_state)
+   {
+      
+      $this->setConfigurationValue('city',$form_state->getValue('city'));
+      $this->setConfigurationValue('desc',$form_state->getValue('desc'));
+      $this->setConfigurationValue('img',$form_state->getValue('img'));
+   }
+   public function build() {
+     $config = $this->getConfiguration();
+     $city= $config['city'];
+     $desc= $config['desc'];
+     $imag= $config['img'];
+     $img= \Drupal\file\Entity\File::load($imag[0]);
+     $path = $img->getFileUri();
+     $service = \Drupal::service('weatherservice');
+     $data=$service->myservice($city);
+     $res=Json::decode($data);
+     return [
+         '#theme' => 'weather',
+         '#city' => $city,
+         '#description'=>$desc,
+         '#image'=> $path,
+         '#min_temp'=>$res['main']['temp_min'],
+         '#max_temp'=>$res['main']['temp_max'],
+         '#pressure' =>$res['main']['pressure'],
+         '#humidity' =>$res['main']['humidity'],
+         '#temp'=>$res['main']['temp']
+     ];
+      }
+
 }
--- web/modules/Custom/weather/src/Services/MyServices.php
+++ web/modules/Custom/weather/src/Services/MyServices.php
@@ -2,11 +2,11 @@
 namespace Drupal\weather\Services;
 class MyServices{
     public $appid;
-    public function myservice()
+    public function myservice($city)
     {
         $appid=\Drupal::config('weather.settings')->get('appid');
         $client=new \GuzzleHttp\Client();
-        $response=$client->request('GET', 'https://samples.openweathermap.org/data/2.5/weather?q=London&appid=b6907d289e10d714a6e88b30761fae22');
+        $response=$client->request('GET', 'https://samples.openweathermap.org/data/2.5/weather?q='.$city.'&appid=b6907d289e10d714a6e88b30761fae22');
         return $response->getBody();
     }
 }
